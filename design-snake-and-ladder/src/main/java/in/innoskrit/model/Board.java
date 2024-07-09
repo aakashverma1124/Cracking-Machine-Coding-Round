958688b10b7b0f6e2be24d4eeac2a06ea51ca44b
Aakash Verma <info.aakash11@gmail.com>
2024-07-09 10:42:39 +0530

    Refactored Snake and Ladder LLD

--- design-snake-and-ladder/src/main/java/in/innoskrit/model/Board.java
+++ design-snake-and-ladder/src/main/java/in/innoskrit/model/Board.java
@@ -1,14 +1,59 @@
 package in.innoskrit.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
 public class Board {
-    private final int size;
-    private final List<Snake> snakes;
-    private final List<Ladder> ladders;
+    private int size;
+    private Dice dice;
+    private List<Snake> snakes;
+    private List<Ladder> ladders;
+    private List<Player> players;
+
+    public Board(int size, Dice dice, List<Snake> snakes, List<Ladder> ladders, List<Player> players) {
+        this.size = size;
+        this.dice = dice;
+        this.snakes = snakes;
+        this.ladders = ladders;
+        this.players = players;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Dice getDice() {
+        return dice;
+    }
+
+    public void setDice(Dice dice) {
+        this.dice = dice;
+    }
+
+    public List<Snake> getSnakes() {
+        return snakes;
+    }
+
+    public void setSnakes(List<Snake> snakes) {
+        this.snakes = snakes;
+    }
+
+    public List<Ladder> getLadders() {
+        return ladders;
+    }
+
+    public void setLadders(List<Ladder> ladders) {
+        this.ladders = ladders;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
 }
